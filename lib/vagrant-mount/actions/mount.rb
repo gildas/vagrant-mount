4039cbc032cf788aedad80fe9a3cafb87d005025
Gildas Cherruel <gildas.cherruel@inin.com>
2015-02-24 07:57:19 +0900

    Started to add action for mount

---
+++ lib/vagrant-mount/actions/mount.rb
@@ -0,0 +1,10 @@
+require 'vagrant'
+require 'vagrant/action/builder'
+
+module VagrantPlugins
+  module Mount
+    module Action
+
+    end
+  end
+end
--- lib/vagrant-mount/plugin.rb
+++ lib/vagrant-mount/plugin.rb
@@ -15,9 +15,50 @@ module VagrantPlugins
       DESC
 
       command(:mount) do
-        require_relative 'command_mount'
+        require_relative 'commands/mount'
         Command::Mount
       end
+      # VirtualBox
+      require_relative 'actions/providers/virtualbox/mount'
+      require_relative 'actions/providers/virtualbox/driver'
+
+      # Hyper-V
+      require_relative 'actions/providers/hyperv/mount'
+      require_relative 'actions/providers/hyperv/driver'
+
+      # VMWare Fusion
+      begin
+        require 'vagrant-vmware-fusion/action'
+        require 'vagrant-vmware-fusion/driver'
+
+        require_relative 'actions/provider/vmware-fusion/mount'
+        require_relative 'actions/provider/vmware-fusion/driver'
+      rescue LoadError
+        # If plugin cannot be loaded, silently ignore
+      end
+
+      # VMWare Workstation
+      begin
+        require 'vagrant-vmware-workstation/action'
+        require 'vagrant-vmware-workstation/driver'
+
+        require_relative 'actions/provider/vmware-workstation/mount'
+        require_relative 'actions/provider/vmware-workstation/driver'
+      rescue LoadError
+        # If plugin cannot be loaded, silently ignore
+      end
+
+      # Parallels Desktop
+      begin
+        require 'vagrant-parallels/action'
+        require 'vagrant-parallels/driver'
+
+        require_relative 'actions/provider/parallels/mount'
+        require_relative 'actions/provider/parallels/driver'
+      rescue LoadError
+        # If plugin cannot be loaded, silently ignore
+      end
+
     end
   end
 end
